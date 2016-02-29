e0db6fe6438a4a7647f9d34e69c0b96923e80d98
Brian Woodward <brian.woodward@gmail.com>
2016-02-29 14:35:25 -0500

    add test file

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+require('mocha');
+var assert = require('assert');
+
+describe('download-stats', function() {
+  it('should work', function() {
+    console.log('implement tests');
+  });
+});
