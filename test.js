4f8867508e77bdd2fe39e2787a0da9e8d7f3f92f
doowb <brian.woodward@gmail.com>
2016-09-26 20:16:53 -0400

    start adding tests

--- test.js
+++ test.js
@@ -2,9 +2,38 @@
 
 require('mocha');
 var assert = require('assert');
+var stats = require('./');
 
 describe('download-stats', function() {
-  it('should work', function() {
-    console.log('implement tests');
+  describe('get', function() {
+    this.timeout(10000);
+
+    it('should get downloads', function(cb) {
+      var downloads = [];
+      stats.get('1900-01-01', '3000-01-01', 'download-stats')
+        .on('data', function(data) {
+          downloads.push(data);
+        })
+        .once('error', cb)
+        .once('end', function() {
+          try {
+            assert(downloads.length > 0, 'expected download data');
+            cb();
+          } catch (err) {
+            cb(err);
+          }
+        });
+    });
+  });
+
+  describe('calculate', function() {
+    it('should calculate total downloads', function() {
+      var downloads = [
+        { downloads: 1 },
+        { downloads: 2 },
+        { downloads: 3 }
+      ];
+      assert.equal(stats.calc.total(downloads), 6);
+    });
   });
 });
