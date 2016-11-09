36805b293a3d502d1794044e7b506573e9c2f03f
doowb <brian.woodward@gmail.com>
2016-11-09 14:49:31 -0500

    add point specific methods for getting downloads update examples and docs

--- examples/get-point.js
+++ examples/get-point.js
@@ -3,13 +3,31 @@
 var stats = require('../');
 
 // get total downloads for the last-month for micromath
-stats.get.point('last-month', 'micromatch')
-  .on('error', console.error)
-  .on('data', function(data) {
-    console.log(data);
-  })
-  .on('end', function() {
-    console.log('done.');
-  });
+stats.get.point('last-month', 'micromatch', function(err, results) {
+  if (err) return console.error(err);
+  console.log('last-month', results);
+});
+// last-month { downloads: 7750788, start: '2016-10-10', end: '2016-11-08', package: 'micromatch' }
 
-// { downloads: 7750788, start: '2016-10-10', end: '2016-11-08', package: 'micromatch' }
+stats.get.allTime('micromatch', function(err, results) {
+  if (err) return console.error(err);
+  console.log('all-time', results);
+});
+
+stats.get.lastMonth('micromatch', function(err, results) {
+  if (err) return console.error(err);
+  console.log('last-month', results);
+});
+// last-month { downloads: 7750788, start: '2016-10-10', end: '2016-11-08', package: 'micromatch' }
+
+stats.get.lastWeek('micromatch', function(err, results) {
+  if (err) return console.error(err);
+  console.log('last-week', results);
+});
+// last-week { downloads: 1777065, start: '2016-11-02', end: '2016-11-08', package: 'micromatch' }
+
+stats.get.lastDay('micromatch', function(err, results) {
+  if (err) return console.error(err);
+  console.log('last-day', results);
+});
+// last-day { downloads: 316004, start: '2016-11-08', end: '2016-11-08', package: 'micromatch' }
